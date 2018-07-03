5b2adbea6b521e67d4bbfb5828c908571acb2a03
Raashid din dar <34117119+raadin7@users.noreply.github.com>
2018-07-03 15:56:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,11 +16,18 @@ switch ( $action ) {
 
 function archive() {
   $results = array();
-  $data = Article::getList();
+  $categoryId = ( isset( $_GET['categoryId'] ) && $_GET['categoryId'] ) ? (int)$_GET['categoryId'] : null;
+  $results['category'] = Category::getById( $categoryId );
+  $data = Article::getList( 100000, $results['category'] ? $results['category']->id : null );
   $results['articles'] = $data['results'];
   $results['totalRows'] = $data['totalRows'];
-  $results['pageTitle'] = "Article Archive | Widget News";
+  $data = Category::getList();
+  $results['categories'] = array();
+  foreach ( $data['results'] as $category ) $results['categories'][$category->id] = $category;
+  $results['pageHeading'] = $results['category'] ?  $results['category']->name : "Article Archive";
+  $results['pageTitle'] = $results['pageHeading'] . " | Widget News";
   require( TEMPLATE_PATH . "/archive.php" );
+  
 }
 
 function viewArticle() {
@@ -31,6 +38,7 @@ function viewArticle() {
 
   $results = array();
   $results['article'] = Article::getById( (int)$_GET["articleId"] );
+  $results['category'] = Category::getById( $results['article']->categoryId );
   $results['pageTitle'] = $results['article']->title . " | Widget News";
   require( TEMPLATE_PATH . "/viewArticle.php" );
 }
@@ -40,6 +48,9 @@ function homepage() {
   $data = Article::getList( HOMEPAGE_NUM_ARTICLES );
   $results['articles'] = $data['results'];
   $results['totalRows'] = $data['totalRows'];
+  $data = Category::getList();
+  $results['categories'] = array();
+  foreach ( $data['results'] as $category ) $results['categories'][$category->id] = $category; 
   $results['pageTitle'] = "Widget News";
   require( TEMPLATE_PATH . "/homepage.php" );
 }
