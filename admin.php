6f522d62586e8929c2385a60056c34e84733dd6d
Raashid din dar <34117119+raadin7@users.noreply.github.com>
2018-07-03 15:57:00 +0530

    Update admin.php

--- admin.php
+++ admin.php
@@ -26,6 +26,18 @@ switch ( $action ) {
   case 'deleteArticle':
     deleteArticle();
     break;
+    case 'listCategories':
+    listCategories();
+    break;
+  case 'newCategory':
+    newCategory();
+    break;
+  case 'editCategory':
+    editCategory();
+    break;
+  case 'deleteCategory':
+    deleteCategory();
+    break;
   default:
     listArticles();
 }
@@ -90,6 +102,8 @@ function newArticle() {
 
     // User has not posted the article edit form yet: display the form
     $results['article'] = new Article;
+    $data = Category::getList();
+    $results['categories'] = $data['results'];
     require( CONTENT_PATH . "/admin/editArticle.php" );
   }
 
@@ -123,6 +137,8 @@ function editArticle() {
 
     // User has not posted the article edit form yet: display the form
     $results['article'] = Article::getById( (int)$_GET['articleId'] );
+    $data = Category::getList();
+    $results['categories'] = $data['results'];
     require( CONTENT_PATH . "/admin/editArticle.php" );
   }
 
@@ -146,6 +162,9 @@ function listArticles() {
   $data = Article::getList();
   $results['articles'] = $data['results'];
   $results['totalRows'] = $data['totalRows'];
+  $data = Category::getList();
+  $results['categories'] = array();
+  foreach ( $data['results'] as $category ) $results['categories'][$category->id] = $category;
   $results['pageTitle'] = "All Articles";
 
   if ( isset( $_GET['error'] ) ) {
@@ -158,6 +177,87 @@ function listArticles() {
   }
 
   require( CONTENT_PATH . "/admin/listArticles.php" );
+}
+
+ 
+ 
+function newCategory() {
+ 
+  $results = array();
+  $results['pageTitle'] = "New Article Category";
+  $results['formAction'] = "newCategory";
+ 
+  if ( isset( $_POST['saveChanges'] ) ) {
+ 
+    // User has posted the category edit form: save the new category
+    $category = new Category;
+    $category->storeFormValues( $_POST );
+    $category->insert();
+    header( "Location: admin.php?action=listCategories&status=changesSaved" );
+ 
+  } elseif ( isset( $_POST['cancel'] ) ) {
+ 
+    // User has cancelled their edits: return to the category list
+    header( "Location: admin.php?action=listCategories" );
+  } else {
+ 
+    // User has not posted the category edit form yet: display the form
+    $results['category'] = new Category;
+    require( TEMPLATE_PATH . "/admin/editCategory.php" );
+  }
+ 
+}
+ 
+ 
+function editCategory() {
+ 
+  $results = array();
+  $results['pageTitle'] = "Edit Article Category";
+  $results['formAction'] = "editCategory";
+ 
+  if ( isset( $_POST['saveChanges'] ) ) {
+ 
+    // User has posted the category edit form: save the category changes
+ 
+    if ( !$category = Category::getById( (int)$_POST['categoryId'] ) ) {
+      header( "Location: admin.php?action=listCategories&error=categoryNotFound" );
+      return;
+    }
+ 
+    $category->storeFormValues( $_POST );
+    $category->update();
+    header( "Location: admin.php?action=listCategories&status=changesSaved" );
+ 
+  } elseif ( isset( $_POST['cancel'] ) ) {
+ 
+    // User has cancelled their edits: return to the category list
+    header( "Location: admin.php?action=listCategories" );
+  } else {
+ 
+    // User has not posted the category edit form yet: display the form
+    $results['category'] = Category::getById( (int)$_GET['categoryId'] );
+    require( TEMPLATE_PATH . "/admin/editCategory.php" );
+  }
+ 
+}
+ 
+ 
+function deleteCategory() {
+ 
+  if ( !$category = Category::getById( (int)$_GET['categoryId'] ) ) {
+    header( "Location: admin.php?action=listCategories&error=categoryNotFound" );
+    return;
+  }
+ 
+  $articles = Article::getList( 1000000, $category->id );
+ 
+  if ( $articles['totalRows'] > 0 ) {
+    header( "Location: admin.php?action=listCategories&error=categoryContainsArticles" );
+    return;
+  }
+ 
+  $category->delete();
+  header( "Location: admin.php?action=listCategories&status=categoryDeleted" );
 }
 
 ?>
