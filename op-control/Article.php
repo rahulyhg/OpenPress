ced912f1b19029d8c6d34ebab564286b8e3ad6ae
Raashid din dar <34117119+raadin7@users.noreply.github.com>
2018-07-03 15:55:57 +0530

    Update Article.php

--- op-control/Article.php
+++ op-control/Article.php
@@ -17,6 +17,10 @@ class Article
   * @var int When the article is to be / was first published
   */
   public $publicationDate = null;
+  /**
+  * @var int The article category ID
+  */
+  public $categoryId = null;
 
   /**
   * @var string Full title of the article
@@ -43,6 +47,7 @@ class Article
   public function __construct( $data=array() ) {
     if ( isset( $data['id'] ) ) $this->id = (int) $data['id'];
     if ( isset( $data['publicationDate'] ) ) $this->publicationDate = (int) $data['publicationDate'];
+    if ( isset( $data['categoryId'] ) ) $this->categoryId = (int) $data['categoryId'];
     if ( isset( $data['title'] ) ) $this->title = preg_replace ( "/[^\.\,\-\_\'\"\@\?\!\:\$ a-zA-Z0-9()]/", "", $data['title'] );
     if ( isset( $data['summary'] ) ) $this->summary = preg_replace ( "/[^\.\,\-\_\'\"\@\?\!\:\$ a-zA-Z0-9()]/", "", $data['summary'] );
     if ( isset( $data['content'] ) ) $this->content = $data['content'];
@@ -99,13 +104,16 @@ class Article
   * @return Array|false A two-element array : results => array, a list of Article objects; totalRows => Total number of articles
   */
 
-  public static function getList( $numRows=1000000, $order="publicationDate DESC" ) {
+  public static function getList( $numRows=1000000, $categoryId=null, $order="publicationDate DESC" ) {
     $conn = new PDO( DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME );
-    $sql = "SELECT SQL_CALC_FOUND_ROWS *, UNIX_TIMESTAMP(publicationDate) AS publicationDate FROM articles
+    $categoryClause = $categoryId ? "WHERE categoryId = :categoryId" : "";
+    $sql = "SELECT SQL_CALC_FOUND_ROWS *, UNIX_TIMESTAMP(publicationDate) AS publicationDate
+            FROM articles $categoryClause
             ORDER BY " . mysql_escape_string($order) . " LIMIT :numRows";
 
     $st = $conn->prepare( $sql );
     $st->bindValue( ":numRows", $numRows, PDO::PARAM_INT );
+    if ( $categoryId ) $st->bindValue( ":categoryId", $categoryId, PDO::PARAM_INT );
     $st->execute();
     $list = array();
 
@@ -136,7 +144,10 @@ class Article
     $sql = "INSERT INTO articles ( publicationDate, title, summary, content ) VALUES ( FROM_UNIXTIME(:publicationDate), :title, :summary, :content )";
     $st = $conn->prepare ( $sql );
     $st->bindValue( ":publicationDate", $this->publicationDate, PDO::PARAM_INT );
+    $st->bindValue( ":categoryId", $this->categoryId, PDO::PARAM_INT );
     $st->bindValue( ":title", $this->title, PDO::PARAM_STR );
+    $st->bindValue( ":title", $this->title, PDO::PARAM_STR );
+    $sql = "INSERT INTO articles ( publicationDate, categoryId, title, summary, content ) VALUES ( FROM_UNIXTIME(:publicationDate), :categoryId, :title, :summary, :content )";
     $st->bindValue( ":summary", $this->summary, PDO::PARAM_STR );
     $st->bindValue( ":content", $this->content, PDO::PARAM_STR );
     $st->execute();
@@ -156,9 +167,11 @@ class Article
    
     // Update the Article
     $conn = new PDO( DB_HOST, DB_USERNAME, DB_PASSWORD );
+    $sql = "UPDATE articles SET publicationDate=FROM_UNIXTIME(:publicationDate), categoryId=:categoryId, title=:title, summary=:summary, content=:content WHERE id = :id";
     $sql = "UPDATE articles SET publicationDate=FROM_UNIXTIME(:publicationDate), title=:title, summary=:summary, content=:content WHERE id = :id";
     $st = $conn->prepare ( $sql );
     $st->bindValue( ":publicationDate", $this->publicationDate, PDO::PARAM_INT );
+    $st->bindValue( ":categoryId", $this->categoryId, PDO::PARAM_INT );
     $st->bindValue( ":title", $this->title, PDO::PARAM_STR );
     $st->bindValue( ":summary", $this->summary, PDO::PARAM_STR );
     $st->bindValue( ":content", $this->content, PDO::PARAM_STR );
