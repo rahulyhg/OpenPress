9400efca78519422dbfc696854b1cec61aa9cda1
Raashid din dar <34117119+raadin7@users.noreply.github.com>
2018-07-03 13:40:57 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# OpenPress
+# OpenPress - A great Content Management System
+OpenPress is an open source CMS. The CMS comes with the latest features and is beginner friendly.
+The CMS hasn't been launched yet but in few months this will be available to public.
+
+#Kindly don't use it on live website because the CMS is under consruction. It is not complete yet but you can test it on localhost using any software like xammp, wamp or mamp, lampetc.
---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+ini_set( "display_errors", true );
+define( "DB_HOST", "localhost" );
+define( "DB_USERNAME", "username" );
+define( "DB_PASSWORD", "password" );
+define("DB_NAME","wgcms")
+define( "CONTROL_PATH", "op-control" );
+define( "CONTENT_PATH", "op-content" );
+define("ADMIN_PATH","op-admin");
+define( "HOMEPAGE_NUM_ARTICLES", 5 );
+define( "ADMIN_USERNAME", "admin" );
+define( "ADMIN_PASSWORD", "mypass" );
+require( CLASS_PATH . "/Article.php" );
+
+function handleException( $exception ) {
+  echo "Sorry, a problem occurred. Please try later.";
+  error_log( $exception->getMessage() );
+}
+
+set_exception_handler( 'handleException' );
+?>
